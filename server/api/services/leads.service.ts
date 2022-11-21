e97ad2b5dd3755d5aa6f4b4edabe0de1278ccb09
Michael Kubín <michael.kubn@gmail.com>
2022-11-21 10:29:53 +0100

    add: notes

--- server/api/services/leads.service.ts
+++ server/api/services/leads.service.ts
@@ -1,41 +1,66 @@
-import {PipedriveDeal, PipedriveOrganization, PipedrivePerson} from "../../pipedrive/pipedrive";
-import pipedriveApi from "../../pipedrive/pipedrive";
+import {
+  PipedriveDeal,
+  PipedriveNote,
+  PipedriveOrganization,
+  PipedrivePerson,
+} from '../../pipedrive/pipedrive';
+import pipedriveApi from '../../pipedrive/pipedrive';
 
 export interface Lead {
-    businessName: string,
-    personName: string,
-    email: string,
-    phone: string,
-    consent: false,
-    currencies: Array<string>
+  businessName: string;
+  personName: string;
+  email: string;
+  phone: string;
+  consent: false;
+  currencies: Array<string>;
 }
 
 class RegistrationResult {
-    constructor(code: number, message: string) {
-        this.code = code;
-        this.message = message;
-    }
+  constructor(code: number, message: string) {
+    this.code = code;
+    this.message = message;
+  }
 
-    code: number;
-    message: string;
+  code: number;
+  message: string;
 }
 
 export class LeadService {
-    async create(lead: Lead): Promise<RegistrationResult> {
-        const org : PipedriveOrganization = new PipedriveOrganization(lead.businessName);
-        const createdOrg = await pipedriveApi.createOrganization(org);
-        if(!createdOrg.id) {
-            return new RegistrationResult(0, "Incorrect organization registration")
-        }
-        const person : PipedrivePerson = new PipedrivePerson(lead.personName, createdOrg.id, lead.email, lead.phone);
-        const createdPerson = await pipedriveApi.createPerson(person, createdOrg);
-        const deal : PipedriveDeal = new PipedriveDeal(createdOrg.name);
-        const createdDeal = await pipedriveApi.createDeal(deal, createdPerson, createdOrg);
-        if(!createdDeal.id) {
-            return new RegistrationResult(0, "Incorrect deal registration");
-        }
-        return new RegistrationResult(1, "OK");
+  async create(lead: Lead): Promise<RegistrationResult> {
+    const org: PipedriveOrganization = new PipedriveOrganization(
+      lead.businessName
+    );
+    const createdOrg = await pipedriveApi.createOrganization(org);
+    if (!createdOrg.id) {
+      return new RegistrationResult(0, 'Incorrect organization registration');
     }
+    const person: PipedrivePerson = new PipedrivePerson(
+      lead.personName,
+      createdOrg.id,
+      lead.email,
+      lead.phone
+    );
+    const createdPerson = await pipedriveApi.createPerson(person, createdOrg);
+    const deal: PipedriveDeal = new PipedriveDeal(createdOrg.name);
+    const createdDeal = await pipedriveApi.createDeal(
+      deal,
+      createdPerson,
+      createdOrg
+    );
+    const note: PipedriveNote = new PipedriveNote(
+      `Zustimmung zur Verarbeitung von personenbezogenen Daten: ${
+        lead.consent
+      }, 
+      Der Kunde ist an Währungen interessiert: ${lead.currencies.join(', ')}`
+    );
+    if (createdDeal.id) {
+      await pipedriveApi.createNote(note, createdDeal.id);
+    }
+    if (!createdDeal.id) {
+      return new RegistrationResult(0, 'Incorrect deal registration');
+    }
+    return new RegistrationResult(1, 'OK');
+  }
 }
 
 export default new LeadService();
--- server/pipedrive/pipedrive.ts
+++ server/pipedrive/pipedrive.ts
@@ -1,143 +1,181 @@
-import axios, {Axios} from "axios";
-import {response} from "express";
-//import l from '../common/logger'
+import axios, { Axios } from 'axios';
 
 export class PipedriveException {
-    code: number;
-    message: string;
+  code: number;
+  message: string;
 
-    constructor(code: number, message: string) {
-        this.code = code;
-        this.message = message;
-    }
+  constructor(code: number, message: string) {
+    this.code = code;
+    this.message = message;
+  }
 }
 
 export class PipedriveObject {
-    id?: number;
-    name: string;
+  id?: number;
+  name: string;
 
-    constructor(name: string, id?: number) {
-        this.id = id;
-        this.name = name;
-    }
+  constructor(name: string, id?: number) {
+    this.id = id;
+    this.name = name;
+  }
 }
 
-export class PipedrivePerson extends PipedriveObject{
-    name: string;
-    organization: number;
-    primaryEmail: string;
-    phone: string;
+export class PipedrivePerson extends PipedriveObject {
+  name: string;
+  organization: number;
+  primaryEmail: string;
+  phone: string;
 
-    constructor(name: string, organization: number, primaryEmail: string, phone: string,  id?: number) {
-        super(name, id);
-        this.organization = organization;
-        this.primaryEmail = primaryEmail;
-        this.phone = phone;
-    }
+  constructor(
+    name: string,
+    organization: number,
+    primaryEmail: string,
+    phone: string,
+    id?: number
+  ) {
+    super(name, id);
+    this.organization = organization;
+    this.primaryEmail = primaryEmail;
+    this.phone = phone;
+  }
 }
 
-export class PipedriveOrganization extends PipedriveObject{
-    name: string;
-    ownerId: number | null;
+export class PipedriveNote {
+  note: string;
 
-    constructor(name: string, ownerId?: number, id?: number) {
-        super(name, id);
-        this.ownerId = ownerId || null;
-    }
+  constructor(note: string) {
+    this.note = note;
+  }
+}
+
+export class PipedriveOrganization extends PipedriveObject {
+  name: string;
+  ownerId: number | null;
+
+  constructor(name: string, ownerId?: number, id?: number) {
+    super(name, id);
+    this.ownerId = ownerId || null;
+  }
 }
 
 export class PipedriveDeal extends PipedriveObject {
-    status = "Open";
+  status = 'Open';
 }
 
 export class PipedriveApiOptions {
-    apiToken: string;
-    companyDomain: string;
+  apiToken: string;
+  companyDomain: string;
 }
 
 export class PipedriveApi {
-    private axios: Axios;
+  private axios: Axios;
 
-    constructor(pipedriveApiOptions : PipedriveApiOptions) {
-        this.axios = axios.create({
-            baseURL: `https://${pipedriveApiOptions.companyDomain}.pipedrive.com/api/v1`,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        this.axios.interceptors.request.use(config => {
-            config.params = { ... config.params, 'api_token': pipedriveApiOptions.apiToken };
-            return config;
-        })
+  constructor(pipedriveApiOptions: PipedriveApiOptions) {
+    this.axios = axios.create({
+      baseURL: `https://${pipedriveApiOptions.companyDomain}.pipedrive.com/api/v1`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    this.axios.interceptors.request.use((config) => {
+      config.params = {
+        ...config.params,
+        api_token: pipedriveApiOptions.apiToken,
+      };
+      return config;
+    });
+  }
+
+  async createDeal(
+    deal: PipedriveDeal,
+    person: PipedrivePerson,
+    organization: PipedriveOrganization
+  ): Promise<PipedriveDeal> {
+    const createResult = await this.axios.post('deals', {
+      title: organization.name,
+      user_id: organization.ownerId ?? undefined,
+      org_id: organization.id,
+      person_id: person.id,
+      visible_to: '3',
+      status: deal.status,
+    });
+    const responseData = createResult.data;
+    if (responseData.success) {
+      const id = responseData.data.id;
+      // const ownerId = responseData.data.owner_id.id;
+
+      return new PipedriveDeal(organization.name, id);
+    } else {
+      throw new PipedriveException(1000, `General exception`);
     }
+  }
 
-    async createDeal(deal : PipedriveDeal, person : PipedrivePerson, organization : PipedriveOrganization) : Promise<PipedriveDeal> {
-        const createResult = await this.axios.post('deals', {
-            title: organization.name,
-            user_id: organization.ownerId ?? undefined,
-            org_id: organization.id,
-            person_id: person.id,
-            visible_to: '3',
-            status: deal.status
-        });
-        const responseData = createResult.data;
-        if(responseData.success) {
-            console.log(JSON.stringify(responseData.data))
-            const id = responseData.data.id;
-            // const ownerId = responseData.data.owner_id.id;
+  async createOrganization(
+    organization: PipedriveOrganization
+  ): Promise<PipedriveOrganization> {
+    const createResult = await this.axios.post('organizations', {
+      name: organization.name,
+      owner_id: organization.ownerId ?? undefined,
+      visible_to: '3',
+    });
+    const responseData = createResult.data;
+    if (responseData.success) {
+      const id = responseData.data.id;
+      const ownerId = responseData.data.owner_id.id;
 
-            return new PipedriveDeal(organization.name, id);
-        }
-        else {
-            throw new PipedriveException(1000, `General exception`);
-        }
+      return new PipedriveOrganization(organization.name, ownerId, id);
+    } else {
+      throw new PipedriveException(1000, `General exception`);
     }
+  }
 
-    async createOrganization(organization: PipedriveOrganization): Promise<PipedriveOrganization> {
-        const createResult = await this.axios.post('organizations', {
-            name: organization.name,
-            owner_id: organization.ownerId ?? undefined,
-            visible_to: '3'
-        });
-        const responseData = createResult.data;
-        if(responseData.success) {
-            const id = responseData.data.id;
-            const ownerId = responseData.data.owner_id.id;
+  async createPerson(
+    person: PipedrivePerson,
+    organization: PipedriveOrganization
+  ) {
+    const createResult = await this.axios.post('persons', {
+      name: person.name,
+      owner_id: organization.ownerId,
+      org_id: organization.id,
+      email: [person.primaryEmail],
+      phone: [person.phone],
+      marketing_status: 'subscribed',
+    });
+    const responseData = createResult.data;
+    if (responseData.success) {
+      const id = responseData.data.id;
+      const ownerId = responseData.data.owner_id.id;
 
-            return new PipedriveOrganization(organization.name, ownerId, id);
-        }
-        else {
-            throw new PipedriveException(1000, `General exception`);
-        }
+      return new PipedrivePerson(
+        person.name,
+        ownerId,
+        person.primaryEmail,
+        person.phone,
+        id
+      );
+    } else {
+      throw new PipedriveException(1000, `General exception`);
     }
+  }
 
-    async createPerson(person: PipedrivePerson, organization: PipedriveOrganization) {
-        const createResult = await this.axios.post('persons', {
-            name: person.name,
-            owner_id: organization.ownerId,
-            org_id: organization.id,
-            email: [person.primaryEmail],
-            phone: [person.phone],
-            marketing_status: 'subscribed'
-        })
-        const responseData = createResult.data;
-        if(responseData.success) {
-            const id = responseData.data.id;
-            const ownerId = responseData.data.owner_id.id;
-
-            return new PipedrivePerson(person.name, ownerId, person.primaryEmail, person.phone, id);
-        }
-        else {
-            throw new PipedriveException(1000, `General exception`);
-        }
-
+  async createNote(note: PipedriveNote, id: PipedriveDeal['id']) {
+    const createResult = await this.axios.post('notes', {
+      content: note.note.toString(),
+      deal_id: id,
+    });
+    const responseData = createResult.data;
+    if (responseData.success) {
+      return new PipedriveNote(note.note);
+    } else {
+      throw new PipedriveException(1000, `General exception`);
     }
+  }
 }
 
-if(!process.env.PIPEDRIVE_TOKEN || !process.env.PIPEDRIVE_DOMAIN) {
-    throw "Missing PIPEDRIVE_TOKEN and PIPEDRIVE_DOMAIN env params"
+if (!process.env.PIPEDRIVE_TOKEN || !process.env.PIPEDRIVE_DOMAIN) {
+  throw 'Missing PIPEDRIVE_TOKEN and PIPEDRIVE_DOMAIN env params';
 }
 export default new PipedriveApi({
-    apiToken: process.env.PIPEDRIVE_TOKEN,
-    companyDomain: process.env.PIPEDRIVE_DOMAIN
+  apiToken: process.env.PIPEDRIVE_TOKEN,
+  companyDomain: process.env.PIPEDRIVE_DOMAIN,
 });
